c0533908a58090ebc7f0e766e5bf83fd1a489046
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-02-21 18:01:41 +0100

    Finished pipeline from S3 to redis

--- build.sbt
+++ build.sbt
@@ -8,4 +8,5 @@ libraryDependencies ++= Seq("com.amazonaws" % "aws-java-sdk" % "1.3.32",
                             "software.amazon.awssdk" % "core" % "2.16.3",
                             "com.github.seratch" %% "awscala" % "0.8.+",
                             "org.json4s" %% "json4s-native" % "3.7.0-M8",
-                            "org.json4s" %% "json4s-jackson" % "3.7.0-M8")
+                            "org.json4s" %% "json4s-jackson" % "3.7.0-M8",
+                            "com.redislabs" % "jredistimeseries" % "1.4.0")
--- src/main/scala/S3Connector/ListS3Files.scala
+++ src/main/scala/S3Connector/ListS3Files.scala
@@ -5,32 +5,43 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream
 import s3._
 
 import java.io.{BufferedReader, InputStreamReader}
-
+import com.redislabs.redistimeseries._
 
 class ListS3Files {
+  // TODO: Load as external parameters
   val TargetBucketName: String = "valena1databucket"
   val BucketPrefix: String = "database/raw/"
   val S3Region: Region = Region.US_EAST_2
 
+  // Init S3
   implicit val s3: S3 = S3.at(S3Region)
   val TargetBucket: Bucket = s3.bucket(TargetBucketName).get
 
+  // Connect to redis
+  val RedisConnector: RedisTimeSeries = new RedisTimeSeries("localhost", 6379)
+
   // Get all json files in target
   for (file <- TargetBucket.keys(BucketPrefix) if file.endsWith(".json")) {
-    println(file)
+
     // Get file input stream
     val S3JsonObject: S3Object = TargetBucket.getObject(file).get
     val DataStream: S3ObjectInputStream = S3JsonObject.getObjectContent
     val InputStream = new BufferedReader(new InputStreamReader(DataStream))
     // Get content and parse to Map Object
     val FileContent = jsonStrToMap.parseJson(InputStream.readLine())
-    println(FileContent)
+
     // Get Date of json and write all other keys to a redis time series
     val JsonDate = FileContent("date")
-    for ((k,v) <- FileContent if k != "date") {
-      println(s"TS.ADD $k $JsonDate $v")
-    }
+    //TODO: Is there a more elegant way?
+    val vJsonDate: Long = JsonDate.toString.toLong
 
+    for ((k,v) <- FileContent if k != "date") {
+      //TODO: Is there a more elegant way?
+      val vDouble: Double = v.toString.toDouble
+
+      RedisConnector.add(k, vJsonDate, vDouble)
+    }
+    println(s"Pared $file with content $FileContent")
   }
 
 }

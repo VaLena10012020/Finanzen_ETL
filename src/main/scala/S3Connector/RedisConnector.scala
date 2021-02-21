1467aa4fd2d4ddb1ff591a861852f406c480fd13
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-02-21 18:42:08 +0100

    Small refactoring

---
+++ src/main/scala/S3Connector/RedisConnector.scala
@@ -0,0 +1,21 @@
+package S3Connector
+
+import com.redislabs.redistimeseries.RedisTimeSeries
+
+case class RedisConnector() {
+
+  val RedisConnector: RedisTimeSeries = new RedisTimeSeries("localhost",  6379)
+
+  def parseMapToS3(MapFile: Map[String, Any]): Unit ={
+    // Get Date of json and write all other keys to a redis time series
+    val JsonDate = MapFile("date")
+    //TODO: Is there a more elegant way?
+    val vJsonDate: Long = JsonDate.toString.toLong
+
+    for ((k,v) <- MapFile if k != "date") {
+      //TODO: Is there a more elegant way?
+      val vDouble: Double = v.toString.toDouble
+      RedisConnector.add(k, vJsonDate, vDouble)
+    }
+  }
+}

b52c41f9eb1933684a9fd5a094b71271ee675150
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-04-11 12:20:21 +0200

    setup travis like Finanzen_Entgelt

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,2 +1,23 @@
-sbt ecr:login \
-    ecr:push
+#!/bin/bash
+
+set -e # exit immediately in case of error
+set -x # print all executed commands (=debug mode)
+
+
+echo "=== Get latest docker image ==="
+
+# get latest docker image for caching if available
+docker pull ${ECR_REGISTRY}/${ECR_REPOSITORY}:finanzen_etl || true
+
+
+echo "=== Build new docker image ==="
+
+docker build --pull=true --cache-from ${ECR_REGISTRY}/${ECR_REPOSITORY}:finanzen_etl \
+  -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:finanzen_etl_${TRAVIS_BRANCH} \
+  --build-arg ECR_REGISTRY=${ECR_REGISTRY} --build-arg ECR_REPOSITORY=${ECR_REPOSITORY} .
+
+# to do add test script for docker image
+
+echo "=== Push docker image ${TRAVIS_BRANCH} to AWS ECR ==="
+
+docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:finanzen_etl_{TRAVIS_BRANCH}

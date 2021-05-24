a4d730a71cc98dc7f5bab0bade7cde442bdb768d
Valentin <valentin.gabriel.kuhn@outlook.de>
2021-05-24 14:10:27 +0200

    delete pull image from build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -5,16 +5,10 @@ set -x # print all executed commands (=debug mode)
 
 name="finanzen_etl"
 
-echo "=== Get latest docker image ==="
-
-# get latest docker image for caching if available
-docker pull ${ECR_REGISTRY}/${ECR_REPOSITORY}:finanzen_etl || true
-
-
 echo "=== Build new docker image ==="
 
 docker build --pull=true --cache-from ${ECR_REGISTRY}/${ECR_REPOSITORY}:finanzen_etl \
-  -t name --build-arg ECR_REPOSITORY=${ECR_REPOSITORY} .
+  -t ${name} --build-arg ECR_REPOSITORY=${ECR_REPOSITORY} .
 
 # to do add test script for docker image
 

70896aac1bb77e0c1ead73acfae0b2165ccfb7a5
Valentin <valentin.gabriel.kuhn@outlook.de>
2021-05-24 17:56:14 +0200

    Parse redis port and host from env vars

--- src/main/scala/com/valena/S3Redis/main.scala
+++ src/main/scala/com/valena/S3Redis/main.scala
@@ -8,14 +8,16 @@ object main extends App{
   val TargetBucketName: String = "valena1databucket"
   val BucketPrefix: String = "database/raw/"
   val S3Region: Region = Region.US_EAST_2
-  val RedisPort: Int = 6379
+  val RedisPort: Int = sys.env.get("redis_port").getOrElse("6379").toInt
+  val RedisHost: String = sys.env.get("redis_host").getOrElse("localhost")
+  println(s"Got Variables for run: RedisHost=$RedisHost, RedisPort=$RedisPort")
 
   // Init S3
   implicit val s3: S3 = S3.at(S3Region)
   val TargetBucket: Bucket = s3.bucket(TargetBucketName).get
 
   // Init RedisConnector
-  implicit val redisConnector: RedisConnector = RedisConnector(RedisPort)
+  implicit val redisConnector: RedisConnector = RedisConnector(RedisHost, RedisPort)
 
   // Start Pipeline
   val pipe = new ParseS3ToRedis
